8841085da726dff97fc4624de550edd8ba5337d5
Arthur Manenti <97462232+noname697@users.noreply.github.com>
2026-04-09 14:28:58 -0300

    [fix]: Arrumando problema de horário

--- atividadeTres.ts
+++ atividadeTres.ts
@@ -1,8 +1,8 @@
 const saudacao = (nome: string, hora: number): string => {
   if (hora < 24 && hora >= 0) {
-    if (hora < 12) {
+    if (hora < 12 && hora > 4) {
       return `Bom dia ${nome}`;
-    } else if (hora < 18) {
+    } else if (hora < 18 && hora >= 12) {
       return `Boa tarde ${nome}`;
     } else {
       return `Boa noite ${nome}`;
@@ -13,4 +13,4 @@ const saudacao = (nome: string, hora: number): string => {
 };
 
 
-console.log(saudacao("Arthur", 12))
+console.log(saudacao("Arthur", 4))
